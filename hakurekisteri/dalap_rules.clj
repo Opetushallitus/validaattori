263cde76c7a24b61bf8f4435c05c2d92ac0d4466
Juha Paananen <juha.paananen@gmail.com>
2015-03-19 15:35:02 +0200

    add dalap definition for IFn

--- hakurekisteri/dalap_rules.clj
+++ hakurekisteri/dalap_rules.clj
@@ -1,4 +1,4 @@
 {
   ["src/validator/core.clj" "gen-cljs/validator/core.cljs"] []
   ["src/validator/ruleset.clj" "gen-cljs/validator/ruleset.cljs"] []
-  ["hakurekisteri/src/hakurekisteri/perusopetus.clj" "gen-cljs/hakurekisteri/perusopetus.cljs"] []}
+  ["hakurekisteri/src/hakurekisteri/perusopetus.clj" "gen-cljs/hakurekisteri/perusopetus.cljs"] ['clojure.lang.IFn 'IFn]}
