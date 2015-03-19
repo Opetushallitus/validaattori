7b8aee15e110e178d6c28dcf60122aa80c8e1d04
Juha Paananen <juha.paananen@gmail.com>
2015-03-19 16:17:34 +0200

    add artifactory repositories

--- project.clj
+++ project.clj
@@ -30,6 +30,8 @@
                                                                   :output-to "target/prod/hakurekisteri-validator.min.js"
                                                                   :optimizations :advanced
                                                                   :pretty-print false}}]}}}
+  :repositories [["snapshots" "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"]
+                 ["releases" "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"]]
   :hooks [leiningen.dalap]
   :cljsbuild {:builds [{:source-paths ["gen-cljs"]
                         :compiler {:output-to "target/javascripts/validator.js"  ; default: target/cljsbuild-main.js
