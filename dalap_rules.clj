c47a3bea35c16935dfece84a62045c5bae44ca3c
Verneri Åberg <verneri.aberg@gmail.com>
2015-03-24 13:23:26 +0200

    fix dalap rules

--- dalap_rules.clj
+++ dalap_rules.clj
@@ -1,4 +1,4 @@
 {
-  ["src/validator/core.clj" "gen-cljs/validator/core.cljs"] []
-  ["src/validator/ruleset.clj" "gen-cljs/validator/ruleset.cljs"] []
+  ["src/clojure/validator/core.clj" "gen-cljs/validator/core.cljs"] []
+  ["src/clojure/validator/ruleset.clj" "gen-cljs/validator/ruleset.cljs"] []
 }
